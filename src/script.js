47858ccb7c21a00472ae2f157cefd204db36c3ca
Vinicius <vini.pessoa7@gmail.com>
2020-04-22 23:48:22 -0400

    feat: text processing

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
 function set_text(txt) {
     document.getElementById("txtOutput").value += txt;
 }
+
+function clean_text() {
+    document.getElementById("txtOutput").value = '';
+}
+
+function process_text() {  
+    txt     = document.getElementById("txtOutput").value;
+    pattern = /\d+[\+\-\*\/]\d+/m;
+    str     = txt.match(pattern);
+    
+    clean_text();
+    alert(txt +"\n"+ str);
+} 
