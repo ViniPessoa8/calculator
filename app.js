d020319759473cf1a8e91833148c43b2e511d248
Vinicius <vini.pessoa7@gmail.com>
2020-04-26 18:02:26 -0400

    feat: port listener

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'src')));
+
+app.listen(PORT, () => console.log("Listening port:", PORT));
